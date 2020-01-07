ed15dbd8897b7d570d6c16de77afa801900c96f1
Melemchuk <melemchuk@yandex.ru>
2020-01-07 09:30:32 +0300

    Change

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo -c /home/box/web/etc/gunicorn-django.conf ask.wsgi_application
